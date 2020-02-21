764b4126fecea6c896cc464ca129e6827e020bc3
jmt62 <jmt62@duke.edu>
2020-02-21 18:00:08 -0500

    Delete load.sql

--- load.sql
+++
@@ -1,6 +0,0 @@
--- Some initial data to play with.  These INSERT statements should succeed.
--- Do NOT modify these.
-INSERT INTO PossibleGoods VALUES('apple', 'fruit', '28');
-INSERT INTO PossibleGoods VALUES('banana', 'fruit', '4');
-INSERT INTO PossibleGoods VALUES('carrot', 'vegetable', '14');
-INSERT INTO User VALUES(0, 'Jonathan Yu', 'jy178', 'password0');
