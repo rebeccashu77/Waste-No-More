525d5c124aa1fa1e5f42ea64fb1be7926e3e55c9
gtm7 <gtm7@duke.edu>
2020-02-23 17:20:29 -0500

    Update User to Users

--- load.sql
+++ load.sql
@@ -6,11 +6,11 @@ INSERT INTO PossibleGoods VALUES('carrot', 'vegetable', 14);
 INSERT INTO PossibleGoods VALUES('eggs', 'protein', 28);
 INSERT INTO PossibleGoods VALUES('bread', 'grain', 7);
 
-INSERT INTO User VALUES(0, 'Jonathan Yu', 'jy178@duke.edu', 'jy178', 'password', 0, 0, 1, 1, 0, 0);
-INSERT INTO User VALUES(1, 'Jeevan Tewari', 'jmt62@duke.edu', 'jmt62', 'password', 1, 0, 0, 1, 1, 0);
-INSERT INTO User VALUES(2, 'Rebecca Shu', 'rjs73@duke.edu', 'rjs73', 'password', 0, 0, 0, 0, 0, 0);
-INSERT INTO User VALUES(3, 'Christina Le', 'cli58@duke.edu', 'cli58', 'password', 0, 1, 0, 0, 1, 1);
-INSERT INTO User VALUES(4, 'Griffin Malm', 'gtm7@duke.edu', 'gtm7', 'password', 0, 1, 0, 1, 1, 1);
+INSERT INTO Users VALUES(0, 'Jonathan Yu', 'jy178@duke.edu', 'jy178', 'password', 0, 0, 1, 1, 0, 0);
+INSERT INTO Users VALUES(1, 'Jeevan Tewari', 'jmt62@duke.edu', 'jmt62', 'password', 1, 0, 0, 1, 1, 0);
+INSERT INTO Users VALUES(2, 'Rebecca Shu', 'rjs73@duke.edu', 'rjs73', 'password', 0, 0, 0, 0, 0, 0);
+INSERT INTO Users VALUES(3, 'Christina Le', 'cli58@duke.edu', 'cli58', 'password', 0, 1, 0, 0, 1, 1);
+INSERT INTO Users VALUES(4, 'Griffin Malm', 'gtm7@duke.edu', 'gtm7', 'password', 0, 1, 0, 1, 1, 1);
 
 INSERT INTO Items VALUES(1, 'apple', '2020-02-15', 10, '2020-03-14');
 INSERT INTO Items VALUES(3, 'banana', '2020-02-15', 8, '2020-02-19');
