0034fdd2b617717803adb1bd35127257568b2db0
gtm7 <gtm7@duke.edu>
2020-02-23 19:06:37 -0500

    Hardcoded variables

--- test-sample.sql
+++ test-sample.sql
@@ -4,19 +4,19 @@
 
 SELECT id
 FROM User
-WHERE username = **username_input**;
+WHERE username = gtm7;
 
 --VIEW ACCOUNT INFO
 
 SELECT name, email, username
 FROM User
-WHERE User.id = **id_var**;
+WHERE User.id = 1;
 
 --DIETARY RESTRICTIONS
 
 SELECT pescatarian, vegetarian, vegan, dairy-free, gluten-free, peanut-allergy
 FROM User
-WHERE User.id = **id_var**;
+WHERE User.id = 2;
 
 --WHEN DETERMINING RECIPES → query all recipes with no violations for dietary restriction
 -- → then query based on the ingredient that is going to expire first in their fridge
@@ -24,10 +24,10 @@ WHERE User.id = **id_var**;
 WITH AvailableRecipes AS
 (SELECT Recipe.name, Recipe.cuisine, ingredient1, ingredient2, ingredient3, ingredient4, ingredient5
 FROM Recipe, User
-WHERE User.id = **id_var** AND User.dairy-free <= Recipe.dairy-free AND User.gluten-free <= Recipe.gluten-free AND User.peanut-allergy <= Recipe.peanut-allergy AND User.pescatarian <= Recipe.pescatarian AND User.vegetarian <= Recipe.vegetarian AND User.vegan<= Recipe.vegan),
+WHERE User.id = 3 AND User.dairy-free <= Recipe.dairy-free AND User.gluten-free <= Recipe.gluten-free AND User.peanut-allergy <= Recipe.peanut-allergy AND User.pescatarian <= Recipe.pescatarian AND User.vegetarian <= Recipe.vegetarian AND User.vegan<= Recipe.vegan),
 firstExpired AS (SELECT T1.food FROM (SELECT food, days_to_expiration
 FROM Items
-WHERE Items.id = **id_var** AND days_to_expiration <= 3
+WHERE Items.id = 3 AND days_to_expiration <= 3
 ORDER BY days_to_expiration DESC, food ASC) AS T1)
 SELECT *
 FROM AvailableRecipes, firstExpired
@@ -37,5 +37,5 @@ WHERE ingredient1 IN (SELECT food FROM firstExpired) OR ingredient2 IN (SELECT f
 
 SELECT food, days_to_expiration
 FROM Items
-WHERE Items.id = **id_var**
-ORDER BY days_to_expiration DESC, food ASC
+WHERE Items.id = 3
+ORDER BY days_to_expiration DESC, food ASC;
