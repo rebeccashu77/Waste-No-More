1380061ad93635f19f3f44162ebb53b028a83dd6
gtm7 <gtm7@duke.edu>
2020-02-23 19:56:30 -0500

    Update test-sample.sql

--- test-sample.sql
+++ test-sample.sql
@@ -27,9 +27,9 @@ FROM Recipe, Users
 WHERE Users.id = 3 AND Users.dairy_free <= Recipe.dairy_free AND Users.gluten_free <= Recipe.gluten_free AND Users.peanut_allergy <= Recipe.peanut_allergy AND Users.pescatarian <= Recipe.pescatarian AND Users.vegetarian <= Recipe.vegetarian AND Users.vegan<= Recipe.vegan),
 firstExpired AS (SELECT T1.name FROM (SELECT name, expiration_date
 FROM Items
-WHERE Items.id = 3
+WHERE Items.id = 3 AND (CURRENT_DATE >= expiration_date - INTEGER '3')
 ORDER BY expiration_date ASC, name ASC) AS T1)
-SELECT AvailableRecipes.name, cuisine, ingredient1, ingredient2, ingredient3, ingredient4, ingredient5
+SELECT DISTINCT AvailableRecipes.name, cuisine, ingredient1, ingredient2, ingredient3, ingredient4, ingredient5
 FROM AvailableRecipes, firstExpired
 WHERE ingredient1 IN (SELECT name FROM firstExpired) OR ingredient2 IN (SELECT name FROM firstExpired) OR ingredient3 IN (SELECT name FROM firstExpired) OR ingredient4 IN (SELECT name FROM firstExpired) OR ingredient5 IN (SELECT name FROM firstExpired);
 
